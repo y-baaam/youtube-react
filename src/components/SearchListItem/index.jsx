756c326e2b662624a239277bdea69483d04c6895
young beom <dudqja3674@gmail.com>
2023-08-01 15:14:02 +0900

    searchListItem , videoCard 생성
    searchListItem 반응형 css

--- src/components/SearchListItem/index.jsx
+++ src/components/SearchListItem/index.jsx
@@ -1,41 +1,42 @@
 import React from "react";
 import styles from "./index.module.css";
 import moment from "moment";
-// import "moment/locale/ko";
+
 import { useNavigate } from "react-router-dom";
 
-export default function VideoCard({ video }) {
+export default function SearchListItem({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
 
   // 게시한 후 시간이 얼마나 지났는지 나타내기
   const publishDate = publishedAt.substring(0, 10);
   const publishDateDiff = moment(publishDate, "YYYY-MM-DD").fromNow();
-
   const navigate = useNavigate();
 
+  // navigate 를 할 때 state 로 데이터를 같이 보낼 수 있다.
   const onClickIntoVideoDetail = () => {
     navigate(`/videos/watch/${video.id}`, { state: { video } });
   };
 
   return (
-    <li className={styles.videoCard} onClick={onClickIntoVideoDetail}>
+    <li className={styles.searchItem}>
       <img
-        className={styles.videoCardThumbnailImg}
+        className={styles.searchItemThumbnailImg}
         src={thumbnails.medium.url}
         alt=""
         width={thumbnails.medium.width}
         height={thumbnails.medium.height}
-        // onClick={onClickIntoVideoDetail}
+        onClick={onClickIntoVideoDetail}
       />
-      <div className={styles.videoCardDescription}>
-        <div className={styles.videoCardChannelImg} alt={""} src={""} />
+
+      <div className={styles.searchItemDescription}>
+        <div className={styles.searchItemChannelImg} alt={""} src={""} />
         <div
-          className={styles.videoCardTextContainer}
-          // onClick={onClickIntoVideoDetail}
+          className={styles.searchItemTextContainer}
+          onClick={onClickIntoVideoDetail}
         >
-          <div className={styles.videoCardTitle}>{title}</div>
-          <div className={styles.videoCardChannelTitle}>{channelTitle}</div>
-          <div className={styles.videoCardPublishedAt}> {publishDateDiff}</div>
+          <div className={styles.searchItemTitle}>{title}</div>
+          <div className={styles.searchItemChannelTitle}>{channelTitle}</div>
+          <div className={styles.searchItemPublishedAt}> {publishDateDiff}</div>
         </div>
       </div>
     </li>
